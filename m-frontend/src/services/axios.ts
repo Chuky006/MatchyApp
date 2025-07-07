a9b5d135ae7900ae56b618a4126e5b967f4aaf74
Chuky006 <ochukosowho@gmail.com>
2025-07-07 16:29:43 +0100

    fix: enable withCredentials in axios instance to support JWT cookies

--- m-frontend/src/services/axios.ts
+++ m-frontend/src/services/axios.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://matchyapp-7.onrender.com/api",
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
